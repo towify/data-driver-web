a3053e2270db0d543cb7b1b57945f2793b764ed3
xianti-towify <xianti.xiong@towify.com>
2023-01-06 15:26:10 +0800

    fixed(xianti/create_project): update package

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -51,3 +51,5 @@ import 'zone.js'; // Included with Angular CLI.
  * APPLICATION IMPORTS
  */
 import '@angular/localize/init';
+// aws-sdk requires global to exist
+(window as any).global = window;
